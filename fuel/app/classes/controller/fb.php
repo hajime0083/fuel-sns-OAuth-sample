b9ef89c6eda72ce918b1b037830bc94d31b903f7
yagi <yagi@core-tech.jp>
2012-12-11 18:52:12 +0900

    投稿時の認証をユーザートークンからアプリ側のトークンに変更

--- fuel/app/classes/controller/fb.php
+++ fuel/app/classes/controller/fb.php
@@ -33,19 +33,10 @@ class Controller_fb extends Controller_Template
 	public function action_index()
 	{
             $data  = array();
-            $token = "";
-            $user  = "";
             $user_data = "";
             $data['word'] = 'Facebook/index';
             $data['menuClass'] = $this->menuClass;
             $data['pageTitle'] = $this->pageTitle;
-            
-            $user = $this->fb->getUser();
-            if($user){
-                $token = $this->fb->getAccessToken();
-                $this->fb->setAccessToken($token);
-                $user_data = $this->fb->api($user . "?fields=id,name");
-            }
 
             $data['user_data'] = $user_data;
             $this->template->content = View::forge('fb/index',$data);
@@ -61,13 +52,9 @@ class Controller_fb extends Controller_Template
             $data['menuClass'] = $this->menuClass;
             $data['pageTitle'] = $this->pageTitle;
             
-            if(!$this->fb->getUser()){
-                // ログインURLを作るときにscopeを下記にしないと、ユーザーのフィードが更新できない模様
-                $login_url = $this->fb->getLoginUrl(Config::get('facebook.login'));
-                Response::redirect($login_url);
-            }else{
-                Response::redirect(Uri::create('fb/index'));
-            }
+            // ログインURLを作るときにscopeを下記にしないと、ユーザーのフィードが更新できない模様
+            $login_url = $this->fb->getLoginUrl(Config::get('facebook.login'));
+            Response::redirect($login_url);
         }
         
         /*
@@ -77,7 +64,6 @@ class Controller_fb extends Controller_Template
          */
         public function action_post(){
             $data   = array();
-            $token  = "";
             $result = "";
             $post   = "";
             $data['word'] = 'facebook/post';
@@ -86,15 +72,21 @@ class Controller_fb extends Controller_Template
             
             if(Input::post()){
                 // POST時
-                $user = $this->fb->getUser();
+                // テスト用にconfigに記述してますが、適宜変更してください。
+                // 本来はDBから直接取得など。
+                $user = Config::get('facebook.userid');
+                
+                // ユーザーのアクセストークンを使わないので無制限に利用できます。
+                // ここではアプリ側のapp token & seacretで投稿してますが、用途が制限されるため、
+                // ウォール投稿の他の使い方は保障できません。
+                
                 if($user){
-                    $token = $this->fb->getAccessToken();
-                    $this->fb->setAccessToken($token);
                     $post = Input::post('postbox');
                     if(mb_strlen($post)>0 && mb_strlen($post)<1000){
                         try{
                             $result = $this->fb->api("/". $user ."/feed", "post", array(
-                            "message" => $post,
+                            "message" => $post . "TESTtime:" . date("Y/m/d H:i:s"),
+                             "access_token" => Config::get('facebook.init.appId') ."|". Config::get('facebook.init.secret')
                             ));
                         } catch (FacebookApiException $e) {
                             $result    = $e;
@@ -104,14 +96,9 @@ class Controller_fb extends Controller_Template
                         $data['login'] = true;
                     }
                 }
-            }else{
-                // 初期表示
-                if($this->fb->getUser()){
-                    $data['login'] = true;
-                }else{
-                    $data['login'] = false;
-                }
             }
+
+            $data['login'] = true;
             $data['result'] = $result;
             $data['post'] = $post;
             $this->template->content = View::forge('fb/post',$data);
--- fuel/app/views/fb/index.php
+++ fuel/app/views/fb/index.php
@@ -5,14 +5,3 @@
 <dt><?php echo Html::anchor('fb/post','■投稿テスト')?></dt>
 <dd>認証済みログインユーザーのタイムラインに投稿</dd>
 </dl>
-
-<h4>ログインユーザー情報</h4>
-<?php if($user_data <> ""){?>
-    <ul>
-        <li>HOME:<?php echo Html::anchor('http://www.facebook.com/profile.php?id='.$user_data['id'],'http://www.facebook.com/profile.php?id='.$user_data['id'],array('target'=>'_blank'))?></li>
-        <li>user_id:<?php echo $user_data['id'] ?></li>
-        <li>user_name:<?php echo $user_data['name'] ?></li>   
-    </ul>
-<?php }else{ ?>
-<p>登録されていません</p>
-<?php } ?>
